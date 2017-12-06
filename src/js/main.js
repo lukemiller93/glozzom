252104be8dee8181cd136d8f4e3253a822cff835
lukemiller93 <lukem1293@gmail.com>
2017-12-06 15:20:34 -0500

    Updated slick slider script

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,30 @@
 $(function() {
   // Auto play modal video
-  $(".video").click(function () {
+  $(".video").click(function() {
     var theModal = $(this).data("target"),
-    videoSRC = $(this).attr("data-video"),
-    videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
-    $(theModal + ' iframe').attr('src', videoSRCauto);
-    $(theModal + ' button.close').click(function () {
-      $(theModal + ' iframe').attr('src', videoSRC);
+      videoSRC = $(this).attr("data-video"),
+      videoSRCauto =
+        videoSRC +
+        "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
+    $(theModal + " iframe").attr("src", videoSRCauto);
+    $(theModal + " button.close").click(function() {
+      $(theModal + " iframe").attr("src", videoSRC);
     });
   });
 });
 
 // Lightbox Script
 
-$(document).on('click', '[data-toggle="lightbox"]', function (event) {
+$(document).on("click", '[data-toggle="lightbox"]', function(event) {
   event.preventDefault();
   $(this).ekkoLightbox();
 });
 
 // Slick Slider Script
-$('.slider').slick({
-  infinite: true,
-  slideToShow:1,
-  slideToScroll:1
+$(function() {
+  $(".slider").slick({
+    infinite: true,
+    slideToShow: 1,
+    slideToScroll: 1
+  });
 });
